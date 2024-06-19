a61083b2dea9c0c4824458b964ed9fd91fb05a70
Christoph Fichtmüller <cf@cfichtmueller.com>
2024-06-19 18:38:20 +0200

    refactor: extract file

---
+++ internal/engine/state.go
@@ -0,0 +1,82 @@
+package engine
+
+import "sync"
+
+type State struct {
+	mu      sync.Mutex
+	Width   float64
+	Height  float64
+	Cells   []*Cell
+	Players map[string]*Player
+}
+
+func NewState(width, height float64) *State {
+	return &State{
+		Width:   width,
+		Height:  height,
+		Cells:   make([]*Cell, 0),
+		Players: map[string]*Player{},
+	}
+}
+
+func (s *State) Update(dt float64) {
+	s.mu.Lock()
+	newCells := make([]*Cell, 0, len(s.Cells))
+	for _, c := range s.Cells {
+		c.Update(dt)
+		if !c.Agent.Dead {
+			newCells = append(newCells, c)
+		}
+	}
+	s.Cells = newCells
+	for _, p := range s.Players {
+		p.Update(dt)
+		c, ok := intersects(p, s.Cells)
+		if !ok {
+			continue
+		}
+		switch c.Type {
+		case CELL_TYPE_BULLET:
+			p.Die()
+			c.Die()
+		case CELL_TYPE_POWER_VELOCITY:
+			c.Die()
+			p.Agent.MaxVelocity += 5
+		}
+	}
+	s.mu.Unlock()
+}
+
+func (s *State) AddCell(c *Cell) {
+	s.mu.Lock()
+	s.Cells = append(s.Cells, c)
+	s.mu.Unlock()
+}
+
+func (s *State) SpawnPlayer() *Player {
+	s.mu.Lock()
+	p := &Player{
+		ID: randomId(),
+		Agent: &Agent{
+			X:                  s.Width / 2,
+			Y:                  s.Height / 2,
+			Width:              30,
+			Height:             30,
+			MaxVelocity:        50,
+			MaxAngularVelocity: 10,
+			Friction:           10,
+		},
+		Color: "#ff00ff",
+	}
+	s.Players[p.ID] = p
+	s.mu.Unlock()
+	return p
+}
+
+func (s *State) PlayerWithId(id string) *Player {
+	p, ok := s.Players[id]
+	if !ok {
+		return nil
+	}
+	return p
+}
